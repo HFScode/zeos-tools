04140df9b72496fab690752d0894c9ac8914575c
dabatnot <dabatnot@gmail.com>
2016-04-02 03:51:01 +0200

    Add LXDE patch

---
+++ patches/scripts/patchLXDE.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#################################################################################
+# Correct some stuff in LXED
+#################################################################################
+#
+# Add the logout menu
+# Remove xscreensaver
+
+#Be sure that base_img.sh is installed before running this script
+/usr/share/zpkgs/zeos-tools/zexecprior.sh base_img.sh
+
+apt-get -y install  --reinstall lxsession-logout
+apt-get -y remove xscreensaver
