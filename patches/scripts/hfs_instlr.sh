561d102ee0842a7a6cb4dd21d6d58f39c8f4115e
dabatnot <dabatnot@gmail.com>
2016-04-02 03:00:49 +0200

    Added main scripts skel

---
+++ patches/scripts/hfs_instlr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#################################################################################
+# Configure HFSBox post install script
+#################################################################################
+#
+# Install a post install script that is run at the first boot to configure
+# the HFSBox (SP or 15K)
+
+echo "Edit me"
