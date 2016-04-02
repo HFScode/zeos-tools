d01f3d86d9bc4aac23fb99f2c3841d22820f8a32
dabatnot <dabatnot@gmail.com>
2016-04-02 02:46:50 +0200

    base_img.sh revision

--- patches/scripts/base_img.sh
+++ patches/scripts/base_img.sh
@@ -10,10 +10,10 @@
 
 echo "Installing prequisites for base image"
 echo
+
 echo "========================================"
 echo "Adding APT Repos"
 echo "========================================"
-read answer
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:libretro/stable
 add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ wily main restricted universe multiverse" 
@@ -22,32 +22,29 @@ add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ wily-updates main r
 add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ wily-backports main restricted universe multiverse"
 add-apt-repository -y "deb http://archive.canonical.com/ubuntu wily partner"
 echo
+
+
+
 echo "========================================"
 echo "Upgrading"
 echo "========================================"
-read answer
 apt-get update
 apt-get -y upgrade
-
-###Following section disabled as it cause troubles
-###while updating kernel
-#echo
-#echo "========================================"
-#echo "Dist upgrading"
-#echo "========================================"
-#read answer
-#apt-get -y dist-upgrade
-
 echo
+
+
+
 echo "========================================"
 echo "Installing requirements"
 echo "========================================"
-read answer
 apt-get -y install testdisk reiserfsprogs ntfs-3g dosfstools
 apt-get -y install casper ubiquity ubiquity-frontend-gtk ubiquity-slideshow-lubuntu
 apt-get -y install lxde
 apt-get -y install zip rar unzip unrar ssh gedit retroarch retroarch-* libretro-* alsa-base alsa-utils pulseaudio xfce4-mixer terminator adobe-flashplugin  gdebi
 echo
+
+
+
 echo "========================================"
 echo "Cleaning."
 echo "========================================"
