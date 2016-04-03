f88e88dc91725a70cda2ee91b8445aa8dc1ab478
dabatnot <dabatnot@gmail.com>
2016-04-03 20:42:52 +0200

    Add HFSBox splitted deb

--- patches/scripts/hfs_instlr.sh
+++ patches/scripts/hfs_instlr.sh
@@ -1,10 +1,21 @@
 #!/bin/bash 
 
 #################################################################################
-# Configure HFSBox post install script
+# Configure HFSBox and post install script
 #################################################################################
-#
+# Install the emulators and ZSpin directory structure
 # Install a post install script that is run at the first boot to configure
 # the HFSBox (SP or 15K)
 
-echo "Edit me"
+echo "======================================================="
+echo "Installing HFSBox"
+echo "======================================================="
+
+echo "Checking for base installation"
+/usr/share/zpkgs/zeos-tools/zexecprior.sh base_img.sh
+
+echo "Configuring zsnes"
+apt-get -y install zsnes
+echo "Configuring HFSBOX and other emulators"
+dpkg-split --join hfsbox_1.0.1*
+dpkg -i /usr/share/zpgks/zeos-tools/patches/deb/hfsbox-1.0.1_1.0.1_all.deb
--- patches/scripts/patchLXDE.sh
+++ patches/scripts/patchLXDE.sh
@@ -5,10 +5,11 @@
 #################################################################################
 #
 # Add the logout menu
-# Remove xscreensaver
+# Remove xscreensave
 
 #Be sure that base_img.sh is installed before running this script
 /usr/share/zpkgs/zeos-tools/zexecprior.sh base_img.sh
 
 apt-get -y install  --reinstall lxsession-logout
-apt-get -y remove xscreensaver
+apt-get -y remove xscreensaver 
+
